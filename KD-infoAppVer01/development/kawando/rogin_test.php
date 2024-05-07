271e0ef920decbfacaa90743eac64755b0f4620a
Rikitost <kd1300674@st.kobedenshi.ac.jp>
2024-05-07 13:50:28 +0900

    DB接続テスト

--- KD-infoAppVer01/development/kawando/rogin_test.php
+++ KD-infoAppVer01/development/kawando/rogin_test.php
@@ -1,4 +1,4 @@
-<!-- http://localhost/PBI1-B-Humble/KD-infoAppVer01/development/kawando/rogin.html -->
+<!-- http://localhost/PBI1-B-Humble/KD-infoAppVer01/development/kawando/rogin_test.php -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <form method="POST" action="foo.php">
+    <form method="POST" action="test.php">
         <div class="login-container">
             <h2>ログイン</h2>
             <form>
@@ -26,17 +26,17 @@
         $user = 'kobe'; // ユーザー名
         $password = 'denshi'; // パスワード
 
-        // insert文を変数に代入し実行
-        try {
-            $pdo = new PDO($dsn, $user, $password); // データベースに接続
-            $sql = 'insert into users(email_address, user_name, user_pass) values(? ,? ,?)'; // SQL文を変数に代入
-            $stmt = $pdo->query($sql); // SQL文を実行
-        } catch (PDOException $e) {
-            echo '接続に失敗しました'; // 失敗した場合に表示
-            var_dump($e->getMessage()); // エラー内容を出力
-            exit; // プログラムを終了
-            die();
-        }
+        // // insert文を変数に代入し実行
+        // try {
+        //     $pdo = new PDO($dsn, $user, $password); // データベースに接続
+        //     $sql = 'insert into users(email_address, user_name, user_pass) values(? ,? ,?)'; // SQL文を変数に代入
+        //     $stmt = $pdo->query($sql); // SQL文を実行
+        // } catch (PDOException $e) {
+        //     echo '接続に失敗しました'; // 失敗した場合に表示
+        //     var_dump($e->getMessage()); // エラー内容を出力
+        //     exit; // プログラムを終了
+        //     die();
+        // }
 
         // select文を変数に代入し実行
         try {
