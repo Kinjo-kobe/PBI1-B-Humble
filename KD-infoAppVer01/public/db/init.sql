0090262dc8a213dae61c77f1f9b5d580df137e28
Rikitost <kd1300674@st.kobedenshi.ac.jp>
2024-05-09 12:11:09 +0900

    質問テーブルと投稿テーブルにカラムを追加＆バイト指定の修正

--- KD-infoAppVer01/public/db/init.sql
+++ KD-infoAppVer01/public/db/init.sql
@@ -14,15 +14,19 @@ create table users(
     user_name varchar(255) not null,
     email_address varchar(255) not null,
     user_pass varchar(255) ,
-    profile_title varchar(50) ,
+    profile_title varchar(100) ,
     profile_text varchar(1000)
 );
 # テーブル「questions」を作成
 create table questions(
     question_id int auto_increment primary key,
     user_id int,
-    question_title varchar(50) not null,
+    question_title varchar(100) not null,
     question_text varchar(1000) not null,
+    question_good int default 0,
+    question_code text,
+    question_image_name varchar(100) default null,
+    question_image mediumblob default null,
     question_time timestamp default current_timestamp,
     -- 外部キー制約
     FOREIGN KEY (user_id) REFERENCES users (user_id)
@@ -33,8 +37,12 @@ create table questions(
 create table posts(
     post_id int auto_increment primary key,
     user_id int,
-    post_title varchar(50) not null,
+    post_title varchar(100) not null,
     post_text varchar(1000) not null,
+    post_good int(3) default null,
+    post_code text,
+    post_image_name varchar(100) default null,
+    post_image mediumblob default null,
     post_time timestamp default current_timestamp,
     -- 外部キー制約
     FOREIGN KEY (user_id) REFERENCES users (user_id)
@@ -62,6 +70,11 @@ create table replies(
     -- 参照先をupdate/deleteした際はエラーを返す
     ON DELETE RESTRICT ON UPDATE RESTRICT
 );
+# テーブル「tag」を作成
+create table tag(
+    tag_id int auto_increment primary key,
+    tag_name varchar(100) not null
+);
 # テーブル「tagcontrol」を作成
 create table tagcontrol(
     tagcontrol_id int auto_increment primary key,
@@ -72,20 +85,16 @@ create table tagcontrol(
     FOREIGN KEY (question_id) REFERENCES questions (question_id)
     -- 参照先をupdate/deleteした際はエラーを返す
     ON DELETE RESTRICT ON UPDATE RESTRICT,
-    -- 外部キー制約 question_id
+    -- 外部キー制約 post_id
     FOREIGN KEY (post_id) REFERENCES posts (post_id)
     -- 参照先をupdate/deleteした際はエラーを返す
     ON DELETE RESTRICT ON UPDATE RESTRICT,
-    -- 外部キー制約 question_id
+    -- 外部キー制約 tag_id
     FOREIGN KEY (tag_id) REFERENCES tag (tag_id)
     -- 参照先をupdate/deleteした際はエラーを返す
-    ON DELETE RESTRICT ON UPDATE RESTRICT,
+    ON DELETE RESTRICT ON UPDATE RESTRICT
 );
-# テーブル「tag」を作成
-create table tag(
-    tag_id int auto_increment primary key,
-    tag_name varchar(50) not null
-);
+
 
 # ユーザーテーブルに仮のデータを追加
 insert into users(user_name, email_address, user_pass, profile_title, profile_text) values('kobe taro', 'kobetaro@st.kobedenshi.ac.jp', 'kobetaro0123', 'Hello', 'I am kobe taro');
