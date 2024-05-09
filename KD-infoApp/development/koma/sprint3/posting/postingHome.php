ccb5eb1c5b5d58aca9dbbe9468f64af06e646ef5
Komakawa <kd1333983@st.kobedenshi.ac.jp>
2024-05-09 22:21:53 +0900

    個人フォルダ内で、ヘッダーコンポーネント実装に向けた実験用の型作り

---
+++ KD-infoApp/development/koma/sprint3/posting/postingHome.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Posting Home</title>
+</head>
+<body>
+  <h1>Posting Home</h1>
+  <h1>当アプリのエントリポイント</h1>
+</body>
+</html>
