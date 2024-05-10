5104e279b722ecb29ca614cd1a9328425aa402bd
Komakawa <kd1333983@st.kobedenshi.ac.jp>
2024-05-10 20:25:22 +0900

    add; header, icon, pages

---
+++ KD-infoApp/public/search/searchHome.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Search</title>
+</head>
+<body>
+  <h1>Search画面</h1>
+</body>
+</html>
