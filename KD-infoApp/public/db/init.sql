03cb2b2ad374e3ec81eae8c5bae87874a05ee99a
Rikitost <kd1300674@st.kobedenshi.ac.jp>
2024-05-10 15:34:55 +0900

    UNIQUE制約追加

--- KD-infoApp/public/db/init.sql
+++ KD-infoApp/public/db/init.sql
@@ -13,7 +13,7 @@ create table users(
     user_id int auto_increment primary key ,
     user_name varchar(255) ,
     user_pass varchar(1000) ,
-    email_address varchar(255),
+    email_address varchar(255) UNIQUE,
     profile_title varchar(100) ,
     profile_text varchar(1000)
 );
