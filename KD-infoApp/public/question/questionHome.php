2f426986307d724d7f67f183b6d4c0b803580260
Komakawa <kd1333983@st.kobedenshi.ac.jp>
2024-05-11 01:14:12 +0900

    login, signup、questionhomeにアイコン追加

--- KD-infoApp/public/question/questionHome.php
+++ KD-infoApp/public/question/questionHome.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Question Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questions KD-info</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Font Awesome CSSを追加 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" type="image/png" href="\PBI1-B-Humble\KD-infoApp\public\Components\static\AppIcon\KD-info2.png">
+
+    <style>
+        body {
+            background-color: #333;
+        }
+    </style>
 </head>
 <body>
   <?php
